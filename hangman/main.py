c062a45f560bd93fd47177fe6a4d52965f66631d
ivan-rh22 <115598160+ivan-rh22@users.noreply.github.com>
2022-10-18 13:43:43 -0500

    Start hangman

---
+++ hangman/main.py
@@ -0,0 +1,26 @@
+import re
+
+# Get the answer
+answer = "What's Up, Doc?"
+
+answer = answer.upper()
+
+# Pre-Game setup
+answer_guessed = []
+
+for current_answer_character in answer:
+    if re.search("^[A-Z]$", current_answer_character):
+        answer_guessed.append(False)
+    else:
+        answer_guessed.append(True)
+
+#Game Logic
+num_of_incorrect_guesses = 5
+current_incorrect_guesses = 0
+
+letters_guessed = []
+
+#User Gameplay Logic
+while current_incorrect_guesses < num_of_incorrect_guesses and False in answer_guessed:
+    # display game summary
+    print(f"Number of incorrect guesses remaining: {num_of_incorrect_guesses - current_incorrect_guesses}")
