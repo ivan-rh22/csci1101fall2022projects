67c170cd8926a374b047422db8074b0352b6b57d
ivan-rh22 <115598160+ivan-rh22@users.noreply.github.com>
2022-10-20 13:29:13 -0500

    Finished v1 of Hangman

--- hangman/main.py
+++ hangman/main.py
@@ -24,3 +24,49 @@ letters_guessed = []
 while current_incorrect_guesses < num_of_incorrect_guesses and False in answer_guessed:
     # display game summary
     print(f"Number of incorrect guesses remaining: {num_of_incorrect_guesses - current_incorrect_guesses}")
+
+    print("Guessed letters: ", end="")
+
+    for current_letter_guessed in letters_guessed:
+        print(current_letter_guessed, end=" ")
+   
+    print() #equivelent of endl in c++
+
+    #display puzzle board
+    for current_answer_index in range(len(answer)):
+        if answer_guessed[current_answer_index]:
+            print(answer[current_answer_index], end="")
+        else:
+            print("_", end="")
+    print()
+
+    #Let user guess a letter
+    letter = input("Enter a letter: ")
+    letter = letter.upper()
+
+    #check if user entered a valid letter
+    if re.search("^[A-Z]$", letter) and len(letter)==1 and letter not in letters_guessed:
+        current_letter_index = 0
+
+        for current_letter_index in letters_guessed:
+            if letter < current_letter_guessed:
+                break
+            current_letter_index += 1
+        
+            letters_guessed.insert(current_letter_index, letter)
+
+        #check if letter is in the puzzle
+        if letter in answer:
+            for current_answer_index in range(len(answer)):
+                if letter == answer[current_answer_index]:
+                    #update answer guessed
+                    answer_guessed[current_answer_index] = True
+        else:
+            current_incorrect_guesses += 1
+
+#post-game summary
+if current_incorrect_guesses < num_of_incorrect_guesses:
+    print("Congratulations, you won!")
+
+else:
+    print(f"Sorry, you lost. The answer was {answer}")
