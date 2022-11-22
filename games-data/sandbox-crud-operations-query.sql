9685a721c9bd3c034b1ab3fc80a68ed5cab593d7
ivan-rh22 <115598160+ivan-rh22@users.noreply.github.com>
2022-11-22 13:21:44 -0600

    database new tests

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES(3456789876, 1, 'it was not me');
+
+--UPDATE sandbox SET double_value = -2.123534 WHERE id = 151;
+
+--DELETE FROM sandbox WHERE id = 88;
+
+SELECT * FROM sandbox;
